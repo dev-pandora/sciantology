175125a95875f67819e25069f0a5c506aa62e773
dev-pandora <jonasooms12@gmail.com>
2025-04-01 14:59:25 +0200

    Group code

--- Assets/Scripts/Group.cs
+++ Assets/Scripts/Group.cs
@@ -1,7 +1,40 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class Group : MonoBehaviour
 {
+
+    private List<Character> m_Follower = new List<Character>();
+    private Character m_Leader;
+
+    public Character GetLeader()
+    {
+        return m_Leader;
+    }
+
+    public void SetLeader(Character leader)
+    {
+        m_Leader = leader;
+    }
+
+    public void AddFollower(Character follower)
+    {
+        m_Follower.Add(follower);
+    }
+
+    public void RemoveFollower(Character follower)
+    {
+        m_Follower.Remove(follower);
+    }
+
+    public int GetSize()
+    {
+        return m_Follower.Count;
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
