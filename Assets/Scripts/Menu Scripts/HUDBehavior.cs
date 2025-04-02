9d1ca7519ebafbc76acfb96ab21cbd11b851ad3b
dev-pandora <jonasooms12@gmail.com>
2025-04-02 23:45:18 +0200

    HUD and timer

---
+++ Assets/Scripts/Menu Scripts/HUDBehavior.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel;
+using TMPro;
+using UnityEngine;
+
+public class HUDBehavior : MonoBehaviour
+{
+
+    [SerializeField] 
+    private GameManager m_GameManager;
+    [SerializeField]
+    private TextMeshProUGUI m_TimeText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = m_GameManager.MaxTime - (Time.time - m_GameManager.StartTime);
+        int minutes = Mathf.FloorToInt(time/60);
+        int seconds = Mathf.FloorToInt(time % 60);
+
+        m_TimeText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+
+    }
+}
