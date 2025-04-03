25b4ab3bae4ba9b261316858dea81be68e4b2aab
Matsumya <162137197+Shaeryry@users.noreply.github.com>
2025-04-03 10:44:39 +0200

    YATAAAA

--- Assets/Scripts/Menu Scripts/HUDBehavior.cs
+++ Assets/Scripts/Menu Scripts/HUDBehavior.cs
@@ -11,11 +11,6 @@ public class HUDBehavior : MonoBehaviour
     private TextMeshProUGUI m_TimeText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -23,7 +18,7 @@ public class HUDBehavior : MonoBehaviour
         int minutes = Mathf.FloorToInt(time/60);
         int seconds = Mathf.FloorToInt(time % 60);
 
-        if (time < 0)
+        if (time > 0)
         {
             m_TimeText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
         }
